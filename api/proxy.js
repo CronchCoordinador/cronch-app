94b942cf109201ca5e523bb8486fed1f8dd78c1c
Leidy Vargas <coordinadorcontablecronch@gmail.com>
2026-06-03 22:11:22 -0500

    fix url apps script

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,6 @@
 ﻿import https from "https";
 
 const SHEET = "https://script.google.com/macros/s/AKfycbxRnCigtbWF2hbPXLfIP5I91I8o8sQJgT-UQrtzI6TrJnBo6r1_uUtVHSOvX_hmIFZE/exec";
-
 function get(url) {
   return new Promise((resolve, reject) => {
     https.get(url, { headers: { "User-Agent": "Mozilla/5.0" } }, res => {
