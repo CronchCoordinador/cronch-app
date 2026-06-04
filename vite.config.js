4120a8cde3de7ce7e57a944e545e6b5e444a2087
Leidy Vargas <coordinadorcontablecronch@gmail.com>
2026-06-04 15:49:53 -0500

    Plugin legacy para navegadores muy antiguos

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
-  plugins: [react({ fastRefresh: false })],
+  plugins: [
+    react({ fastRefresh: false }),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+  ],
   build: {
     target: 'es2015',
   },
